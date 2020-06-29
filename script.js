5731144310f893887d1671a7bb4772a8c754f7e8
andytran36 <andytran636@gmail.com>
2020-06-28 18:01:28 -0600

    site overhaul

---
+++ script.js
@@ -0,0 +1,12 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    if (window.innerWidth < 768) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-60px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
